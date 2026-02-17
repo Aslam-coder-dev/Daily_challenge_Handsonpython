255ffe2f6aa9fbb7d1358e8be06dff4e6c5dcd36
Aslam <shaikaslam8793@gmail.com>
2026-02-17 20:32:37 +0530

    Implemented weight categorization and validation logic
    Added classification for weights into categories and handled invalid entries.

--- Day5.py
+++ Day5.py
@@ -14,5 +14,35 @@ if username == "Aslam" and password == "SR310":
 
     print("Entered Weights:", weights)
 
+    very_light = []
+    normal_load = []
+    heavy_load = []
+    overload = []
+    invalid_entries = []
+
+    valid_c = 0
+
+    for w in weights:
+        if w < 0:
+            invalid_entries.append(w)
+        elif w <= 5:
+            very_light.append(w)
+            valid_c += 1
+        elif w <= 25:
+            normal_load.append(w)
+            valid_c += 1
+        elif w <= 60:
+            heavy_load.append(w)
+            valid_c += 1
+        else:
+            overload.append(w)
+            valid_c += 1
+
+    print("Very Light Packages:", very_light)
+    print("Normal Load Packages:", normal_load)
+    print("Heavy Load Packages:", heavy_load)
+    print("Overload Packages:", overload)
+    print("Invalid Entries:", invalid_entries)
+
 else:
     print("Access Denied ?? Unauthorized user")
