b09afeeeb4148846db934fabd754059170be91e7
Aslam <shaikaslam8793@gmail.com>
2026-02-17 16:24:38 +0530

    Implement Smart Transport System login and report

---
+++ Day5.py
@@ -0,0 +1,68 @@
+print("=== Smart Transport System Login ===")
+username = input("Enter username: ")
+password = input("Enter password: ")
+if username == "Aslam" and password == "SR310":
+    print("Access Granted!!\n")
+    n = int(input("Enter no.of weights: "))
+    weights = [0] * n
+    for i in range(n):
+        k = int(input(f"Enter weight {i+1}: "))
+        weights[i] = k
+
+    print("Entered Weights:", weights)
+    very_light = []
+    normal_load = []
+    heavy_load = []
+    overload = []
+    invalid_entries = []
+
+    valid_c = 0
+    affected_c = 0
+    name = "Aslam Shaik"
+    L = len(name.replace(" ", ""))
+    PLI = L % 3
+    for w in weights:
+        if w < 0:
+            invalid_entries=invalid_entries+[w]
+        elif w <= 5:
+            very_light=very_light+[w]
+            valid_c += 1
+        elif w <= 25:
+            normal_load=normal_load+[w]
+            valid_c += 1
+        elif w <= 60:
+            heavy_load=heavy_load+[w]
+            valid_c += 1
+        else:
+            overload=overload+[w]
+            valid_c += 1
+    if PLI == 0:
+        for i in overload:
+            invalid_entries=invalid_entries+[i]
+            affected_c += 1
+        overload = []
+    elif PLI == 1:
+        affected_c = len(very_light)
+        very_light = []
+
+    elif PLI == 2:
+        affected_c = len(very_light) + len(overload)
+        very_light = []
+        overload = []
+    print("\n--- Smart Transport Report ---")
+    print("Name Length (L):", L)
+    print("PLI Value:", PLI)
+    print("Total Valid Packages:", valid_c)
+    print("Packages affected by PLI:", affected_c)
+
+    print("\n--- Final Loading Plan ---")
+    print("Very Light Packages:", very_light)
+    print("Normal Load Packages:", normal_load)
+    print("Heavy Load Packages:", heavy_load)
+    print("Overload Packages:", overload)
+    print("Invalid Entries:", invalid_entries)
+
+    print("\nAll trucks are ready for dispatch")
+
+else:
+    print("Access Denied ?? Unauthorized user")
